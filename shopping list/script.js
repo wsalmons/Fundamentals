362ed86f55cc006e986a40d7bd6116793fbb5268
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-02 16:28:22 -0700

    created shopping list part 1

---
+++ shopping list/script.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+	initShoppingList();
+};
+
+function initShoppingList () {
+	let form = document.getElementById("item-form");
+
+	form.addEventListener("submit", (event) => {
+		handleItemForm(event, form);
+	});
+}
+
+function handleItemForm(event, formRef) {
+	if(event.preventDefault) {
+		event.preventDefault();
+	}
+
+	addItemToShoppingList();
+
+	return false;
+}
+
+function addItemToShoppingList() {
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+	console.log("Item HTML: ", itemHtml)
+	let itemListRef = document.getElementById("shopping-list");
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+}
+
+function createListItemHtml(itemName, itemAmount) {
+	return `<li>
+					${itemName} - ${itemAmount}
+				</li>
+	`;
+}
